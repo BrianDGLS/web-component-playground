79d3c2b6bcce2164bdca2cfdf78d497a28cbcbe8
BrianDGLS <briandgls92@gmail.com>
2018-12-09 11:23:59 +0000

    feature: add extension example

--- examples/main.ts
+++ examples/main.ts
@@ -1,6 +1,6 @@
 import { CustomElement, Attribute } from '../src/main'
 
-class MyCounterComponent extends CustomElement(HTMLElement) {
+class MyCounter extends CustomElement(HTMLElement) {
     static selector = 'my-counter'
     static observedAttributes = ['count']
 
@@ -27,4 +27,61 @@ class MyCounterComponent extends CustomElement(HTMLElement) {
     }
 }
 
-window.customElements.define(MyCounterComponent.selector, MyCounterComponent)
+window.customElements.define(MyCounter.selector, MyCounter)
+
+class MyClock extends CustomElement(HTMLElement) {
+    static selector = 'my-clock'
+    static observedAttributes = ['time']
+
+    template = `<h2 id="time"></h2>`
+    $time!: HTMLElement
+
+    connected() {
+        this.$time = this.querySelector('#time') as HTMLElement
+
+        this.updateTime()
+        setInterval(this.updateTime, 1000)
+    }
+
+    updateTime = () => {
+        this.$time.innerHTML = this.getTimeString()
+    }
+
+    getTimeString() {
+        const { h, m, s } = this.getTime()
+        return h.substr(-2) + ':' + m.substr(-2) + ':' + s.substr(-2)
+    }
+
+    getTime() {
+        const h = '0' + new Date().getHours()
+        const m = '0' + new Date().getMinutes()
+        const s = '0' + new Date().getSeconds()
+        return { h, m, s }
+    }
+}
+
+window.customElements.define(MyClock.selector, MyClock)
+
+class MyHexClock extends MyClock {
+    static selector = 'my-hex-clock'
+
+    connected() {
+        super.connected()
+
+        this.updateHex()
+        this.$time.style.color = 'white'
+
+        setInterval(this.updateHex, 1000)
+    }
+
+    updateHex = () => {
+        this.$time.style.background = this.getHexString()
+    }
+
+    getHexString() {
+        const { h, m, s } = this.getTime()
+        return '#' + h.substr(-2) + '' + m.substr(-2) + '' + s.substr(-2)
+    }
+}
+
+window.customElements.define(MyHexClock.selector, MyHexClock)
